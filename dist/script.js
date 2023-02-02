c2484f88c96eda5b38e0d644fa3341c8bd666ec4
Archit <archit@ashuitec.com>
2023-02-02 14:24:14 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,54 @@
+"use strict";
+var _a;
+const btn = document.getElementById("btn");
+const video = document.getElementById("myVideo");
+const formName = document.querySelector("#name");
+const formEmail = document.querySelector("#email");
+const formMessage = document.querySelector("#message");
+const form = document.querySelector("form");
+let store = [];
+form.addEventListener("input", () => {
+    if (!form.checkValidity()) {
+        console.log(form.checkValidity());
+        btn.classList.remove("disabled");
+    }
+    else {
+        btn.classList.add("disabled");
+        console.log("hiii");
+    }
+});
+form.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target === form) {
+        const fname = formName.value;
+        const femail = formEmail.value;
+        const fmessage = formMessage.value;
+        store.push(`name is ${fname} and email is ${femail} with message ${fmessage}`);
+        console.log(store);
+        console.log();
+    }
+});
+const yourImage = document.querySelectorAll("#your-image-id");
+const displayFrame = document.querySelector("#display-frame");
+const displayImg = document.querySelector("#display-img");
+const clickOnImg = {
+    this: HTMLElement,
+};
+for (let i = 0; i < yourImage.length; i++) {
+    (_a = yourImage[i]) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log(this);
+        displayImg.src = this.src;
+        displayFrame.style.display = "flex";
+    });
+}
+displayFrame === null || displayFrame === void 0 ? void 0 : displayFrame.addEventListener("click", function () {
+    this.style.display = "none";
+});
+const age = new Date().getFullYear() - new Date(845424332000).getFullYear();
+console.log(age);
+class name1 {
+    constructor() {
+        this.name = "rohan";
+    }
+}
---
+++ src/script.ts
@@ -0,0 +1,56 @@
+const btn = document.getElementById("btn") as HTMLButtonElement;
+const video = document.getElementById("myVideo") as HTMLAudioElement;
+const formName = document.querySelector("#name") as HTMLInputElement;
+const formEmail = document.querySelector("#email") as HTMLInputElement;
+const formMessage = document.querySelector("#message") as HTMLInputElement;
+const form = document.querySelector("form") as HTMLFormElement;
+
+let store: (number | string)[] = [];
+form.addEventListener("input", () => {
+  if (!form.checkValidity()) {
+    console.log(form.checkValidity());
+    btn.classList.remove("disabled");
+  } else {
+    btn.classList.add("disabled");
+    console.log("hiii");
+  }
+});
+
+form.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target === form) {
+    const fname = formName.value;
+    const femail = formEmail.value;
+    const fmessage = formMessage.value;
+    store.push(
+      `name is ${fname} and email is ${femail} with message ${fmessage}`
+    );
+    console.log(store);
+    console.log();
+  }
+});
+
+const yourImage = document.querySelectorAll(
+  "#your-image-id"
+) as NodeListOf<Element>;
+const displayFrame = document.querySelector(
+  "#display-frame"
+) as HTMLIFrameElement;
+const displayImg = document.querySelector("#display-img") as HTMLImageElement;
+
+const clickOnImg = {
+  this: HTMLElement,
+};
+
+for (let i = 0; i < yourImage.length; i++) {
+  yourImage[i]?.addEventListener("click", function (this: HTMLImageElement, e) {
+    e.preventDefault();
+    console.log(this);
+    displayImg!.src = this.src;
+    displayFrame!.style.display = "flex";
+  });
+}
+displayFrame?.addEventListener("click", function () {
+  this.style.display = "none";
+});
